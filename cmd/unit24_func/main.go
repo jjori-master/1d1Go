95ab7d571bd08d4b0b245dcc4b330fb4806a8a45
jjori <jjori.master@gmail.com>
2019-08-14 23:06:54 +0900

    function 공부 / 사칙연산으로 예제 실습 중

---
+++ cmd/unit24_func/main.go
@@ -0,0 +1,5 @@
+package unit24_func
+
+func sum(a int, b int) int {
+	return a + b
+}
---
+++ cmd/unit24_func/main_test.go
@@ -0,0 +1,31 @@
+package unit24_func
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestGinkgo(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Test functions Suite")
+}
+
+var _ = Describe("사칙 연슨",
+	func() {
+
+		Context("사칙 연산을 해보자", func() {
+			It("더하기 연산", func() {
+				result := sum(1, 1)
+				Expect(result).Should(Equal(2))
+
+				result = sum(1, 2)
+				Expect(result).Should(Equal(3))
+
+				result = sum(1000, 200)
+				Expect(result).Should(Equal(1200))
+			})
+		})
+	},
+)
